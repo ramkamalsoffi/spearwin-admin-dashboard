47fcd0290d316196f3ba2ff7425d99865e9427ca
GOKULSCSE <gokul03903@gmail.com>
2025-10-22 12:00:29 +0530

    patch

--- src/pages/AddCountry.tsx
+++ src/pages/AddCountry.tsx
@@ -92,6 +92,7 @@ export default function AddCountry() {
                     required
                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                     placeholder="Enter country name"
+                    placeholder="Enter country name"
                   />
                 </div>
 
